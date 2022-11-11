fdaf55a93a70bee1dffb427943c43d48d024d72f
Michelle <scottmm374@gmail.com>
2022-11-11 11:43:55 -0500

    added comments in code

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # Advent_puzzles_scraper
+
 Web scraper- Directories generator to save  Advent of Code puzzles.
 
 [Visit Advent of Code](https://adventofcode.com)
+
+#### Why I created this repo
+
+I created this scraper because I wanted a central place to not only work on the puzzles, but to also save my solutions.
+
+Each year there are 25 puzzles, as well as 25 bonus puzzles for a total of 50. Thats a lot of puzzles to create a folder, and all the files, so I decided to automate it!
 
 What this Repo currently does
 
@@ -9,36 +16,26 @@ What this Repo currently does
 - Creates Sub folders for each puzzle inside each year directory.
 - Creates txt file for puzzle input
 - Creates md file populated with Puzzle instructions scraped from website.
-- Creates a py file (For solving) :) 
-
-
+- Creates a py file (For solving) :)
 
 ![Directories by year](images/year_directories.png)
 ---
+
 ![Alt text](images/puzzle_sub_directories.png)
 ---
+
 ![Alt text](images/puzzle_file.png)
 ---
+
 ![Alt text](images/puzzle_readme.png)
 ---
 
-
 ### What is Advent of Code?
+
 [Visit Advent of Code](https://adventofcode.com)
 
 Advent of code is a website created by Eric Wastl in 2015.
 
-Every year on December 1st a new puzzle is released each day until December 25th. 
+Every year on December 1st a new puzzle is released each day until December 25th.
 
-These puzzles can be solved in any programming language and any skill level can play. 
-
-
-
-#### Why I created this repo.
-I created this scraper because I wanted a central place to not only work on the puzzles, but to also save my solutions. 
-
-Each year there are 25 puzzles, as well as 25 bonus puzzles for a total of 50. Thats a lot of puzzles to create a folder, and all the files, so I decided to automate it! 
-
-
-
-
+These puzzles can be solved in any programming language and any skill level can play.
--- scraper/main.py
+++ scraper/main.py
@@ -1,15 +1,15 @@
 
-from bs4 import BeautifulSoup
-from urllib.error import URLError
 import os
-from pathlib import Path
-import requests
 import time
+from pathlib import Path
+from urllib.error import URLError
+import requests
+from bs4 import BeautifulSoup
 
 
 
 def get_puzzle_text(url):
-
+    # Scraper using bs4
     try:
         response = requests.get(url, allow_redirects=False)
         response.raise_for_status()
@@ -36,45 +36,41 @@ def get_puzzle_text(url):
         
     except:
         print("couldnt find year")
-
+    
+    #  Puzzle titles Used for subfolder and md  file naming 
     file_name = title.text.replace('---', '').strip().replace(' ', '_')
 
     create_directories(puzzle_text, file_name, year)
 
 
 def get_url():
-    start = time.time()
-    time.sleep(15)
-    end = time.time()
-    print(f"Time elapsed, url {(end - start):.5f} seconds")
-
+    #  Function to read and feed each puzzle url to scrape 
+ 
     with open('urls.txt', 'r') as file:
         for url in file:
+            time.sleep(10)
             get_puzzle_text(url.strip())
 
 
 def create_directories(puzzle, title, year):
+    # Creates all the directories, sub-directories, .py, .txt and .md with puzzle instructions
     
+        # Creates directories by year
         directory = str(year)
         os.makedirs(directory, exist_ok=True)
         
+        # Create Sub-directories
         sub_dir = f'{title}'
-
-        # Creates directories
 
         os.makedirs(os.path.join(directory, sub_dir), exist_ok=True)
         os.makedirs(os.path.join(directory, sub_dir), exist_ok=True)
             
-        # Readme with Puzzle instructions
+        # Creates .md with Puzzle instructions
         with open(os.path.join(directory, sub_dir, f'{title}.md'), "w" ) as file:
                 file.write(puzzle)
-
+        # Creates txt and py file for each sub directory
         Path(os.path.join(directory, sub_dir, "input.txt")).touch()
         Path(os.path.join(directory, sub_dir,  "main.py")).touch()
-    
 
-
-
-# get_puzzle_text('https://adventofcode.com/2015/day/1')
 
 get_url()
